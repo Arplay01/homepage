a914869627fa8e8e5e9da59df68cb182c1620721
Arek Paprzycki <arek.working@gmail.com>
2020-08-11 20:00:35 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('Hello!')
+console.log('hello! nice to see you')
+
+const x = 25
+console.log(x)
+let y = 'hi'
+console.log(y)
+/* let y = 32
+console.log(y) */
+
+const dogName = 'Nel'
+const dogAge = 2
+console.log(`Cześć! Poznaj mojego psiaka ${dogName}, która ma ${dogAge} lata. Żałuj, że jej nie znasz... :D `)
