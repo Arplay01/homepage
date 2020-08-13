4ed0439e8c33f7752efa73c0bc1833106fbfa4ee
Arek Paprzycki <arek.working@gmail.com>
2020-08-13 11:11:13 +0200

    add BEM to HTML, Change CSS selector on BEM, Add description to article by querySelector in JS

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ console.log(y)
 /* let y = 32
 console.log(y) */
 
-const dogName = 'Nel'
-const dogAge = 2
-console.log(`Cześć! Poznaj mojego psiaka ${dogName}, która ma ${dogAge} lata. Żałuj, że jej nie znasz... :D `)
+const dogName = 'Nel';
+const dogAge = 2;
+console.log(`Cześć! Poznaj mojego psiaka ${dogName}, która ma ${dogAge} lata. Żałuj, że jej nie znasz... :D `);
+
+const note = document.querySelector('.note__description--js');
+note.innerHTML = 'Ten tekst dodałem dzięki js. Mam nadzieje że to działa, bo jak nie to dupa';
+
+const noteSucceed = document.querySelector('.note__description--js-succeed');
+noteSucceed.innerHTML = `Udało się! A mój pies to ${dogName} i ma ${dogAge} lata.`;
+
+const noteSucceedDescription = document.querySelector('.note__description--js-succeed-description');
+noteSucceedDescription.innerHTML = '🎉🎉 Z programowaniem jest jak z gotowaniem. Serio. Ważne! Uważnie zatrzymać się na początku i precyzyjnie i łopatologicznie ustalić co chcę osiągnąć - jakie aktywności muszę wykonać, w jakiej kolejności. Czego potrzebuję do ugotowania tego rozwiązania? 🏀🎉';
