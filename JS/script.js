de48fb8927a9950bc2ac0096b8c4490f31664bc0
Arek Paprzycki <arek.working@gmail.com>
2020-08-24 20:07:48 +0200

    import/ export practice

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+import {person, hello} from './modules/object.js';
+import maciek from './modules/object.js';
+import {pleaseSayHelloAtStart as hi} from './modules/test.js'
+import * as korsi from './modules/object2.js';
+
+console.log(person);
+
+console.log(hello(person.name));
+
+console.log(maciek);
+
+hi(person.name);
+
+console.log(korsi.person);
+
+console.log(korsi.hello(korsi.person.name));
--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello my best friend - cow');
-
-const hello = () => {
-    console.log('dzień dobry!');
-}
-
-hello();
